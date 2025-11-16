d27671cbd211d4addfb7af92ca1f0c60fed84743
WonderSi <kirillkozlov00@gmail.com>
2025-11-16 07:18:27 +0700

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="app">
+        <div class="app__container">
+            <Header/>
+            <TodoList ref="todoListRef"/> 
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { provide, ref } from 'vue';
+    import Header from '@cmp/Header.vue';
+    import TodoList from '@cmp/TodoList.vue';
+
+    const todoListRef = ref(null);
+
+    provide('todoListRef', todoListRef);
+</script>
+
+<style scoped lang="scss">
+    .app {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        height: 100vh;
+
+        &__container {
+            width: fit-content;
+            height: 100%;
+            position: relative;
+            box-sizing: border-box;
+        }
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+    <header class="header">
+        <h1 class="header__name">TODO LIST</h1>
+        <div class="header__nav">
+            <SearchBar/>
+            <div class="nav__controls">
+                <FiltersDropdown/>
+                <ThemeToggle/>
+            </div>            
+        </div>
+    </header>
+</template>
+
+<script setup>
+    import SearchBar from '@cmp/SearchBar.vue'
+    import FiltersDropdown from '@cmp/FiltersDropdown.vue'
+    import ThemeToggle from '@cmp/ThemeToggle.vue'
+</script>
+
+<style scoped lang="scss">
+    .header {
+        display: flex;
+        flex-direction: column;
+
+        &__name {
+            margin-top: 40px;
+            text-align: center;
+            font-weight: 500;
+        }
+
+        &__nav {
+            display: flex;
+            flex-direction: row;
+            gap: 15px;
+            margin-top: 10px;
+            &__controls {
+                gap: 15px;
+            }
+        }
+    }
+
+    .nav {
+        &__controls {
+            display: flex;
+            gap: 15px;
+        }
+    }
+
+    @include tablet {
+        .header {
+            &__name {
+                margin-top: 20px;
+            }
+
+            &__nav {
+                margin-top: 10px;
+                gap: 10px;
+            }
+        }
+    }
+
+    @include mobile {
+        .header {
+            &__name {
+                margin-top: 10px;
+            }
+
+            &__nav {
+                margin-top: 15px;
+                flex-direction: column;
+                gap: 8px;
+            }
+        }
+
+        .nav {
+            &__controls {
+                gap: 8px;
+            }
+        }
+    }
+</style>
